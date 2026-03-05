fbb34e90932eceef7e1f69667caa5d1fdc5a0bcc
André Wittwer <andre.wittwer@gmail.com>
2026-03-05 13:25:30 +0100

    chore: simplify code

--- src/main/kotlin/org/oife/passport/DownloadablePassportList.kt
+++ src/main/kotlin/org/oife/passport/DownloadablePassportList.kt
@@ -17,7 +17,7 @@ private val jsonOutputFormat = Json { prettyPrint = true }
 fun CombinedPassport.loadDownloadablePassports(): JsonArray =
     buildJsonArray {
         add(Pdf.ALL_PASSPORT_COMBINED)
-        this@loadDownloadablePassports.passportConfigs.sortedBy { it.languageCode }.forEach {
+        passportConfigs.sortedBy { it.languageCode }.forEach {
             add(it.fileName)
         }
     }
